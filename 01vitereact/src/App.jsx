1f9d9e1667e79ac18e8bbedfdc38ab24e1254fe3
ankur_raj <ankur_raj@blackbelthelp.com>
2024-10-24 07:06:25 +0530

    basic concepts

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Project from "./Project"
+
+// Function name should always start with Capital letter
+function App() {
+
+  return (
+    <>
+      <Project/>
+      <h1>Test Paragraph</h1>
+    </>
+ 
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Project.jsx
@@ -0,0 +1,8 @@
+function Project(){
+    return (
+        <h2>My name is Ankur</h2>
+
+    )
+}
+
+export default Project
